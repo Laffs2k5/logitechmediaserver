77c640ac2efc1b391c26502707e796611d9d6c88
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2019-10-11 22:51:52 +0200

    Docker run script: Fix: Adjust to recent changes in Dockerfile and adjust for local needs.

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-docker run -d --name="LogitechMediaServer" \
---net="bridge" \
+docker container rm -f logitechmediaserver_lms_1
+docker run \
+--name="logitechmediaserver_lms_1" \
 -p 3483:3483/tcp \
--p 9000:9000/tcp \
+-p 3483:3483/udp \
+-p 9001:9000/tcp \
 -p 9090:9090/tcp \
--e PUID="99" \
--e PGID="100" \
--e TZ="America/New_York" \
--v "/mnt/user/appdata/LogitechMediaServer":"/config":rw \
--v "/mnt/user/Music":"/music":rw \
--v "/mnt/user/Plugins":"/plugins":rw \
-logitechmediaserver
+-e PUID="1000" \
+-e PGID="1000" \
+-v "/etc/timezone":"/etc/timezone":ro \
+-v "/etc/localtime":"/etc/localtime":ro \
+-v "/opt/logitechmediaserver/config":"/config":rw \
+-v "/opt/logitechmediaserver/music":"/music":rw \
+-v "/opt/logitechmediaserver/plugins":"/plugins":rw \
+-v "/opt/logitechmediaserver/playlist":"/playlist":rw \
+logitechmediaserver_lms_1
